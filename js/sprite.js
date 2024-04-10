54b64ab083d3c80ccf3fd65fee6cbeb2cbc3a52f
CARY JARDIN <cjardin@roughrider.csusm.edu>
2024-04-10 15:48:46 -0700

    sprite 1

--- js/sprite.js
+++ js/sprite.js
@@ -1,12 +1,46 @@
-//Parent Sprit Class
+//Parent Sprit Classa
 class Sprite {
-    constructor(sprite_json, x, y){
+    constructor(sprite_json, x, y, start_state){
         this.sprite_json = sprite_json;
         this.x = x;
         this.y = y;
+        this.state = start_state;
+        this.root_e = "TenderBud";
+
+        this.cur_frame = 0;
+
+        this.x_v = 10;
+        this.y_v = 0;
     }
 
     draw(){
-        console.log("draw 1");
+        var ctx = canvas.getContext('2d');
+        //console.log(this.sprite_json[this.root_e][this.state][this.cur_frame]['w']);
+
+        if(this.sprite_json[this.root_e][this.state][this.cur_frame]['img'] == null){
+            console.log("loading");
+            this.sprite_json[this.root_e][this.state][this.cur_frame]['img'] = new Image();
+            this.sprite_json[this.root_e][this.state][this.cur_frame]['img'].src = 'Penguins/' + this.root_e + '/' + this.state + '/' + this.cur_frame + '.png';
+        }
+        ctx.drawImage(this.sprite_json[this.root_e][this.state][this.cur_frame]['img'], this.x, this.y );
+
+        this.cur_frame = this.cur_frame + 1;
+        if(this.cur_frame >= this.sprite_json[this.root_e][this.state].length){
+            console.log(this.cur_frame);
+            this.cur_frame = 0;
+        }
+
+        this.x = this.x + this.x_v;
+        this.y = this.y + this.y_v;
+
+        if(this.x >= window.innerWidth){
+            this.x_v = -10;
+            this.state = 'walk_W';
+        }
+
+        if(this.x <= 0){
+            this.x_v = 10;
+        }
+        
     }
 }
